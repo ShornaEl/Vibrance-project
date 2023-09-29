2309a8f1d3fa4d9e90f3ed28ef9d0b0c4a52f2c3
ShornaEl <sabrinahossain761@gmail.com>
2023-09-29 19:13:56 +0600

    stresslevel2

--- app/src/main/java/com/example/Stresslevel.java
+++ app/src/main/java/com/example/Stresslevel.java
@@ -3,6 +3,11 @@ package com.example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.vibrance.R;
 
@@ -12,5 +17,37 @@ public class Stresslevel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stresslevel);
+        Button calculateButton = findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateStressLevel();
+            }
+        });
     }
-}
+
+        private void calculateStressLevel() {
+            int stressLevel = 0;
+
+            // Question 1
+            RadioGroup question1RadioGroup = findViewById(R.id.question1RadioGroup);
+            int question1SelectedId = question1RadioGroup.getCheckedRadioButtonId();
+            if (question1SelectedId != -1) {
+                RadioButton question1SelectedRadioButton = findViewById(question1SelectedId);
+                if (question1SelectedRadioButton.getText().equals("Yes")) {
+                    stressLevel += 1;
+                }
+            }
+
+            // Add more questions and calculations for each question here
+
+            // Display the calculated stress level
+            String message = "Your stress level is: " + stressLevel;
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
