85f16ae5886a10330f267f0c56e3642a65c6f596
ShornaEl <sabrinahossain761@gmail.com>
2023-09-29 18:37:56 +0600

    create_stresslevel

--- app/src/main/java/com/example/vibrance/Main_page.java
+++ app/src/main/java/com/example/vibrance/Main_page.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.Stresslevel;
-
 public class Main_page extends AppCompatActivity {
 
     @Override
@@ -18,7 +16,7 @@ public class Main_page extends AppCompatActivity {
         Button stress_btn = (Button) findViewById(R.id.stress_btn);
         //  Button log_in = (Button) findViewById(R.id.login);
         Intent stress;
-        stress = new Intent(Main_page.this, Stresslevel.class);
+      //  stress = new Intent(Main_page.this, Stresslevel.class);
         Intent login_dashboard;
         //  login_dashboard = new Intent(MainActivity.this, splashscreen.class);
         stress_btn.setOnClickListener(new View.OnClickListener() {
